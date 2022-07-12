faf2b13c57802e719798c3ba83b2a7ca281dbf29
sho55555 <shogo61019@gmail.com>
2022-07-12 11:06:06 +0900

    空のメッセージが送信できないバリデーションの設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,13 +6,14 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @room = Room.find(params[:room_id])
-    @message = @room.messages.new(message_params)
-  if @message.save
-    redirect_to room_messages_path(@room)
-  else
-    @messages = @room.messages.includes(:user)
-    render :index
+      @room = Room.find(params[:room_id])
+      @message = @room.messages.new(message_params)
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      @messages = @room.messages.includes(:user)
+      render :index
+    end
   end
 
   private
